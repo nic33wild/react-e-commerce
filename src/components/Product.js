9d209fc470de65c66ab4bc97ed7bf152b17bb745
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-01-23 11:36:41 +0100

    added bootstrap card footer with title and price

--- src/components/Product.js
+++ src/components/Product.js
@@ -23,16 +23,26 @@ export default class Product extends Component {
                 console.log("added to the cart");
               }}
             >
-            {inCart ? (
-              <p className="text-capitalize mb-0" disabled>
-                in cart
-              </p>
-            ) : (
-              <i className="fas fa-cart-plus" />
-            )}
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
             </button>
-            
           </div>
+          {/* bootstrap card footer */}
+          <div className="card-footer d-flex justify-content-between">
+                <p className="align-self-center mb-0">
+                  {title}
+                </p>
+                <h5 className="text-blue font-italic mb-0">
+                  <span className="mr-1">$</span>
+                  {price}
+                </h5>
+          </div>
+
         </div>
       </ProductWrapper>
     );
