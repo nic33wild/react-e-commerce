cf89735ef66af6d0389ad87b8c70af1eb4d3d74e
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-01-23 12:46:14 +0100

    added check propTypes

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
+import PropTypes from 'prop-types'
 
 export default class Product extends Component {
   render() {
@@ -47,6 +48,17 @@ export default class Product extends Component {
       </ProductWrapper>
     );
   }
+}
+
+Product.propTypes = {
+  product:PropTypes.shape({
+    id:PropTypes.number,
+    img:PropTypes.string,
+    title:PropTypes.string,
+    price:PropTypes.number,
+    inCart:PropTypes.bool
+  }).isRequired
+
 }
 
 const ProductWrapper = styled.div`
