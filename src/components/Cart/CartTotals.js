3183fecd8a1829811ff4bd337ef0e68e2912f268
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-01-30 12:56:22 +0100

    added paypal button with environment variables without specify sandboxappID

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PayPalButton from "./PayPalButton";
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
@@ -12,29 +13,28 @@ export default function CartTotals({ value }) {
               <button
                 className="btn btn-outline-danger text-uppercase mb-3 px-5"
                 type="button"
-                onClick={()=>clearCart()}
+                onClick={() => clearCart()}
               >
                 clear cart
               </button>
             </Link>
             <h5>
-                <span className="text-title">
-                    subtotal:
-                </span>
-                <strong>€ {cartSubTotal}</strong>
+              <span className="text-title">subtotal:</span>
+              <strong>€ {cartSubTotal}</strong>
             </h5>
             <h5>
-                <span className="text-title">
-                    tax:
-                </span>
-                <strong>€ {cartTax}</strong>
+              <span className="text-title">tax:</span>
+              <strong>€ {cartTax}</strong>
             </h5>
             <h5>
-                <span className="text-title">
-                    total:
-                </span>
-                <strong>€ {cartTotal}</strong>
+              <span className="text-title">total:</span>
+              <strong>€ {cartTotal}</strong>
             </h5>
+            <PayPalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
           </div>
         </div>
       </div>
